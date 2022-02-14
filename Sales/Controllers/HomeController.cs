5ab8230692c0180a968c6d40bf1595f7c7b02291
Edgar Rubén Rodríguez Reyes <edgar.reyes@outlook.com>
2022-02-14 06:57:59 -0600

    Add project files.

---
+++ Sales/Controllers/HomeController.cs
@@ -0,0 +1,226 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sales.Models;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Formatting = System.Xml.Formatting;
+using System.Text.RegularExpressions;
+
+namespace Sales.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="BadInputFormat"></param>
+        /// <param name="Receipt"></param>
+        /// <returns></returns>
+        public IActionResult Index(bool BadInputFormat = false, string? Receipt = null)
+        {
+            var itemsList = GetItemList();
+            ViewBag.BadInputFormat = (BadInputFormat) ? "true" : null;
+            ViewBag.Receipt = Receipt;
+            return View(itemsList);
+        }
+
+        #region MethodByForm
+
+        /// <summary>
+        /// Saves the new item if it's valid
+        /// </summary>
+        /// <param name="newItem"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult SaveItemForm(ItemModel newItem)
+        {
+            if (ModelState.IsValid)
+            {
+                SaveNewItem(newItem);
+            }
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Receipt
+
+        /// <summary>
+        /// Generates the receipt and callback the result
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult PrintReceiptForm()
+        {
+            return RedirectToAction("Index", new { Receipt = GenerateReceipt() });
+        }
+
+        /// <summary>
+        /// Generates the receipt with the required format
+        /// </summary>
+        /// <returns></returns>
+        private string GenerateReceipt()
+        {
+            var receipt = string.Empty;
+            var itemsList = GetItemList();
+            var itemGroups = itemsList?.DistinctBy(x => x.Name);
+            var itemCount = 0;
+            var groupTotal = 0.00M;
+            var receiptLine = string.Empty;
+            var importedLegend = string.Empty;
+            var countLegend = string.Empty;
+
+            foreach (var group in itemGroups)
+            {
+                itemCount = itemsList.Where(x => x.Name == group.Name).Count();
+                groupTotal = itemsList.Sum(x => x.FinalPrice);
+                importedLegend = (group.Imported) ? "Imported " : string.Empty;
+                countLegend = (itemCount > 1) ? $" ({itemCount} @ {group.FinalPrice})" : string.Empty;
+                receiptLine = $"{importedLegend}{group.Name}: {itemCount * group.FinalPrice}{countLegend}\n";
+                receipt += receiptLine;
+            }
+
+            receipt += $"Sales Taxes: {itemsList.Sum(x => x.RoundedSalesTax + x.RoundedImportedTax)}\n";
+            receipt += $"Total: {itemsList.Sum(x => x.Total)}\n";
+
+            return receipt;
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// Retrieves items list add the new one and save changes in items.json
+        /// </summary>
+        /// <param name="newItem"></param>
+        private void SaveNewItem(ItemModel newItem)
+        {
+            var itemList = GetItemList();
+            itemList?.Add(newItem);
+            SaveItems(itemList);
+        }
+
+        #region MethodByInputLine
+
+        /// <summary>
+        /// Determines if an item is Imported or not
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private bool IsImported(string name)
+        {
+            return name.Contains("Imported");
+        }
+
+        /// <summary>
+        /// Determines if an item is exempted of Sales Tax
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        private bool IsExempted(string name)
+        {
+            string pattern = @"(book|chocolate|pill)";
+            Regex rgx = new Regex(pattern);
+            Match match = rgx.Match(name.ToLower());
+
+            return match.Success;
+        }
+
+        private string GetItemName(string name)
+        {
+            return name.Replace("Imported", string.Empty);
+        }
+
+        /// <summary>
+        /// Uses regex to valid and convert input line to a ItemModel object. Calls SaveNewItem method to add the new Item and save changes in item.json file
+        /// </summary>
+        /// <param name="Input"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult SaveInputForm(string Input)
+        {
+            if (Input == null) return RedirectToAction("Index", new { BadInputFormat = true });
+            string pattern = @"(?<quantity>[0-9]+) (?<name>.*) (at) (?<price>[0-9]+.[0-9]+)";
+            bool BadInputFormat = false;
+            Regex rgx = new Regex(pattern);
+            Match match = rgx.Match(Input);
+            if (match.Success)
+            {
+                Group quantity = match.Groups["quantity"];
+                Group name = match.Groups["name"];
+                Group price = match.Groups["price"];
+
+                var newItem = new ItemModel
+                {
+                    Imported = IsImported(name.Value),
+                    Exempted = IsExempted(name.Value),
+                    Name = GetItemName(name.Value),
+                    Quantity = int.Parse(quantity.Value),
+                    Price = decimal.Parse(price.Value),
+                };
+
+                SaveNewItem(newItem);
+            }
+            else
+            {
+                BadInputFormat = true;
+            }
+
+            return RedirectToAction("Index", new { BadInputFormat });
+        }
+
+        #endregion
+
+        #region JsonFileHandlers
+
+        /// <summary>
+        /// Removes all lines from items.json file
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult RemoveItemsForm()
+        {
+            System.IO.File.WriteAllText(@"Database\items.json", string.Empty);
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Reads item.json file and deserialize to an ItemModel list to handle it
+        /// </summary>
+        /// <returns>List of ItemModel</returns>
+        private List<ItemModel>? GetItemList()
+        {
+            var jsonFile = System.IO.File.ReadAllText(@"Database\items.json");
+            return JsonConvert.DeserializeObject<List<ItemModel>>(jsonFile) ?? new List<ItemModel>();
+        }
+
+        /// <summary>
+        /// Save a item ist into items.json file
+        /// </summary>
+        /// <param name="itemList"></param>
+        private void SaveItems(List<ItemModel>? itemList)
+        {
+            var convertedJson = JsonConvert.SerializeObject(itemList);
+            System.IO.File.WriteAllText(@"Database\items.json", convertedJson);
+        }
+
+        #endregion
+    }
+}
---
+++ Sales/Models/ItemModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Sales.Models
+{
+    public class ItemModel
+    {
+        private static readonly decimal SALES_TAX_RATE_PERCENTAGE = 10;
+        private static readonly decimal IMPORT_TAX_RATE_PERCENTAGE = 5;
+        private static readonly decimal ROUND_DECIMAL = 0.05M;
+
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string? Name { get; set; }
+        public bool Imported { get; set; }
+        public bool Exempted { get; set; }
+
+        public decimal SalesTax => (Exempted) ? 0 : Price * (SALES_TAX_RATE_PERCENTAGE / 100);
+
+        public decimal RoundedSalesTax => Math.Ceiling(SalesTax / ROUND_DECIMAL) * ROUND_DECIMAL;
+
+        public decimal ImportedTax => (Imported) ? Price * (IMPORT_TAX_RATE_PERCENTAGE / 100) : 0;
+
+        public decimal RoundedImportedTax => Math.Ceiling(ImportedTax / ROUND_DECIMAL) * ROUND_DECIMAL;
+
+        public decimal FinalPrice => Price + RoundedSalesTax + RoundedImportedTax;
+
+        public decimal Total => Quantity * FinalPrice;
+    }
+}
